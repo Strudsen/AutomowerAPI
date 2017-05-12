7fb138c1d645b6be61dea93fe54cabcc8949c133
Kasper Kristensen <kk@firegeeks.com>
2017-05-12 17:49:13 +0200

    version 0.0.3

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup
 
 setup(
    name='AutomowerAPI',
-   version='0.0.1',
+   version='0.0.3',
    description='python API to connect to Husqvarna Automower',
    author='Kasper Kristensen',
    author_email='kk@kasperk.it',
    packages=['AutomowerAPI'],
-   install_requires=['requests', 'json', 'logging'],
 )
