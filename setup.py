a7a715542ffb92df5f2cef3d34a33fbe56259469
Kasper Kristensen <kk@firegeeks.com>
2017-05-12 18:27:26 +0200

    edit setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
+import io
 from setuptools import setup
 
 setup(
    name='AutomowerAPI',
    version='0.0.3',
    description='python API to connect to Husqvarna Automower',
+   long_description=io.open('README.md', encoding='UTF-8').read(),
    author='Kasper Kristensen',
    author_email='kk@kasperk.it',
    packages=['AutomowerAPI'],
+   url='https://github.com/Strudsen/AutomowerAPI/',
 )
